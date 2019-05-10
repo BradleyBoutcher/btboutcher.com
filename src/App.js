c8aa7f7c0d5756ba8dffcc05185a4f08a5b6825f
Bradley <bboutcher@vmware.com>
2019-05-10 11:11:51 -0400

    Modify initial page with custom jumbotron Initial commit to start removing boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 import './App.css';
+
+var Style = {
+  header: {
+    margin: "15px"
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Jumbotron style={Style.header}>
+        <h1>Bradley Boutcher</h1>
+      </Jumbotron>
   );
 }
 
